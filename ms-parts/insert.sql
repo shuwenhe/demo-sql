085491d775b8fa54e3af7d04d5ba48fa3e1c0170
何书文 <1201220707@pku.edu.cn>
2019-12-18 14:31:45 +0800

    add

---
+++ ms-parts/insert.sql
@@ -0,0 +1,3 @@
+INSERT  INTO "banners" ("id","app","imag_url","order","link","color","client_type") VALUES ('15a2915c1f934ebd8ac480fa433940d2','mid','https://imgs.wanmen.org/123.png',2,'https://www.wanmen.org/courses/456','#0d008c',2) RETURNING "banners"."id"
+INSERT  INTO "sales_fission_courses" ("sales_fissions_id","course_id","lecture_id") VALUES (161,'dbb0efb3-1ea8-4901-8d1d-969bd85bef68','b66e4201-d8b3-4ee4-8b7a-edbe29aeba71') RETURNING "sales_fission_courses"."id"
+INSERT  INTO "sales_fissions_courses" ("sales_fissions_id","course_id","lecture_id") VALUES (161,'dbb0efb3-1ea8-4901-8d1d-969bd85bef68','b66e4201-d8b3-4ee4-8b7a-edbe29aeba71') RETURNING "sales_fissions_courses"."id"
